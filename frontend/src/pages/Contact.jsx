2f4813fa3f4ee2d3d2cd090de253a980ca5bd690
Afnane <elidrissiafnane@gmail.com>
2026-01-17 17:01:23 +0100

    remov jsx on style  in contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -216,7 +216,7 @@ const Contact = ({ isDark }) => {
       </main>
 
       {/* Animations */}
-      <style jsx>{`
+      <style >{`
         @keyframes fadeIn {
           from { opacity: 0; transform: translateY(-20px); }
           to { opacity: 1; transform: translateY(0); }
